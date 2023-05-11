16b88c03c3616e7c3eb7d054c6d591d35bf06eff
nym <alinamontma@gmail.com>
2023-05-11 19:42:02 +0200

    reservar fxml

--- src/javafxmlapplication/ReservarFXMLController.java
+++ src/javafxmlapplication/ReservarFXMLController.java
@@ -6,7 +6,13 @@ package javafxmlapplication;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 
 /**
  * FXML Controller class
@@ -14,6 +20,59 @@ import javafx.fxml.Initializable;
  * @author nymok
  */
 public class ReservarFXMLController implements Initializable {
+
+    @FXML
+    private Label title_num_pista_label;
+    @FXML
+    private TextField find_hour_textfield;
+    @FXML
+    private Button find_hour_button;
+    @FXML
+    private Button nine_ten_button;
+    @FXML
+    private Button ten_eleven_button;
+    @FXML
+    private Button eleven_twelve_button;
+    @FXML
+    private Button twelve_thirteen_button;
+    @FXML
+    private Button thirteen_fourteen_button;
+    @FXML
+    private Button fourteen_fifthteen_button;
+    @FXML
+    private Button fithteen_sixteen_button;
+    @FXML
+    private Button sixteen_seventeen_button;
+    @FXML
+    private Button seventeen_eighteen_button;
+    @FXML
+    private Button eighteen_nineteen_button;
+    @FXML
+    private Button nineteen_twenty_button;
+    @FXML
+    private Button twenty_twentyone_button;
+    @FXML
+    private Button twentyone_twentytwo_button;
+    @FXML
+    private Button check_button;
+    @FXML
+    private Button back_button;
+    @FXML
+    private Button exit_button;
+    @FXML
+    private DatePicker calendar_date_picker;
+    @FXML
+    private ToggleButton pista1_toogle_button;
+    @FXML
+    private ToggleButton pista4_toggle_button;
+    @FXML
+    private ToggleButton pista2_toggle_button;
+    @FXML
+    private ToggleButton pista5_toggle_button;
+    @FXML
+    private ToggleButton pista3_toggle_button;
+    @FXML
+    private ToggleButton pista6_toggle_button;
 
     /**
      * Initializes the controller class.
