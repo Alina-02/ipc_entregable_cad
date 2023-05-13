5990cb333e3faca32128170b979bc096301e866a
nym <alinamontma@gmail.com>
2023-05-13 08:50:13 +0200

    fechas y toggle group de pistas

--- src/javafxmlapplication/ReservarFXMLController.java
+++ src/javafxmlapplication/ReservarFXMLController.java
@@ -5,10 +5,13 @@
 package javafxmlapplication;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -22,11 +25,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.OverrunStyle;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import model.Booking;
 import model.Club;
+
 
 /**
  * FXML Controller class
@@ -48,17 +54,9 @@ public class ReservarFXMLController implements Initializable {
     @FXML
     private Button eleven_twelve_button;
     @FXML
-    private Button twelve_thirteen_button;
-    @FXML
-    private Button thirteen_fourteen_button;
-    @FXML
     private Button fourteen_fifthteen_button;
     @FXML
-    private Button fithteen_sixteen_button;
-    @FXML
     private Button sixteen_seventeen_button;
-    @FXML
-    private Button seventeen_eighteen_button;
     @FXML
     private Button eighteen_nineteen_button;
     @FXML
@@ -74,8 +72,6 @@ public class ReservarFXMLController implements Initializable {
     @FXML
     private DatePicker calendar_date_picker;
     @FXML
-    private ToggleButton pista1_toogle_button;
-    @FXML
     private ToggleButton pista4_toggle_button;
     @FXML
     private ToggleButton pista2_toggle_button;
@@ -85,6 +81,94 @@ public class ReservarFXMLController implements Initializable {
     private ToggleButton pista3_toggle_button;
     @FXML
     private ToggleButton pista6_toggle_button;
+    
+    @FXML
+    private GridPane nine_ten_gridpane;
+    @FXML
+    private Label nine_ten_ocupado_label;
+    @FXML
+    private GridPane nine_ten_gridpane1;
+    @FXML
+    private Label nine_ten_ocupado_label1;
+    @FXML
+    private VBox nine_ten_vbox;
+    @FXML
+    private VBox ten_eleven_vbox;
+    @FXML
+    private GridPane nine_ten_gridpane2;
+    @FXML
+    private VBox eleven_twelve_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label2;
+    @FXML
+    private Button twelve_thirdteen_button;
+    @FXML
+    private GridPane nine_ten_gridpane3;
+    @FXML
+    private VBox twelve_thirdteen_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label3;
+    @FXML
+    private Button thirdteen_fourteen_button;
+    @FXML
+    private GridPane nine_ten_gridpane4;
+    @FXML
+    private VBox thirdteen_fourteen_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label4;
+    @FXML
+    private GridPane nine_ten_gridpane5;
+    @FXML
+    private VBox fourteen_fifthteen_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label5;
+    @FXML
+    private Button fifthteen_sixteen_button;
+    @FXML
+    private GridPane nine_ten_gridpane6;
+    @FXML
+    private VBox fifthteen_sixteen_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label6;
+    @FXML
+    private GridPane nine_ten_gridpane7;
+    @FXML
+    private VBox sixteen_seventeen_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label7;
+    @FXML
+    private Button seventeen_eigtheen_button;
+    @FXML
+    private VBox seventeen_eighteen_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label8;
+    @FXML
+    private GridPane nine_ten_gridpane9;
+    @FXML
+    private VBox eighteen_nineteen_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label9;
+    @FXML
+    private GridPane nine_ten_gridpane10;
+    @FXML
+    private VBox nineteen_twenty_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label10;
+    @FXML
+    private GridPane nine_ten_gridpane11;
+    @FXML
+    private VBox twenty_twentyone_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label11;
+    @FXML
+    private Button twentyone_twentytwo_button;
+    @FXML
+    private GridPane nine_ten_gridpane12;
+    @FXML
+    private VBox twentyone_twentytwo_vbox;
+    @FXML
+    private Label nine_ten_ocupado_label12;
+    
 
     /**
      * Initializes the controller class.
@@ -92,17 +176,13 @@ public class ReservarFXMLController implements Initializable {
     
     Hashtable used = new Hashtable();
     @FXML
-    private GridPane nine_ten_gridpane;
-    @FXML
-    private Label nine_ten_ocupado_label;
-    @FXML
-    private Button nine_ten_button1;
-    @FXML
-    private GridPane nine_ten_gridpane1;
-    @FXML
-    private Label nine_ten_ocupado_label1;
-    @FXML
-    private VBox nine_ten_vbox;
+    private ToggleButton pista1_toggle_button;
+    
+    
+    //VARIABLES
+    
+    ToggleGroup pistas = new ToggleGroup(); //grupo pistas
+    LocalDate date = LocalDate.now(); //fecha de las pistas
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -110,6 +190,27 @@ public class ReservarFXMLController implements Initializable {
         try{
         Club club = Club.getInstance();
         }catch(Exception e){}
+        
+        pista1_toggle_button.setToggleGroup(pistas);
+        pista2_toggle_button.setToggleGroup(pistas);
+        pista3_toggle_button.setToggleGroup(pistas);
+        pista4_toggle_button.setToggleGroup(pistas);
+        pista5_toggle_button.setToggleGroup(pistas);
+        pista6_toggle_button.setToggleGroup(pistas);
+
+        pista1_toggle_button.setSelected(true);
+       
+
+        //getCourtBookings(String courtName, LocalDate madeForDay)
+        calendar_date_picker.setOnAction(e -> {
+            date = calendar_date_picker.getValue();
+        });
+        
+        //ArrayList<Booking> today = getCourtBookings("pista 1", date);
+        
+        //comprobar qué horas del día están ocupadas y cambiar los botones
+        //correspondientes :)
+
         
         used.put(910, false);
         used.put(1011, false);
@@ -123,19 +224,18 @@ public class ReservarFXMLController implements Initializable {
         used.put(1920, false);
         used.put(2021, false);
         used.put(2122, false);
-    }    
+        
+        
+        
+    } 
+    
+    
 
     @FXML
     private void exit_clicked(MouseEvent event) {
         Platform.exit();
     }
 
-    @FXML
-    private void nine_ten_clicked(MouseEvent event) {
-        
-        
-        
-    }
     
     
     
@@ -149,8 +249,41 @@ public class ReservarFXMLController implements Initializable {
         nickname.setPadding(new Insets(0, 10, 0, 0));
         vbox.getChildren().add(1,nickname);
     }
+
+    @FXML
+    private void hour_clicked(MouseEvent event) {
+        
+    }
+
+    @FXML
+    private void pista1_toggle_button_clicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void pista4_toggle_button_clicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void pista2_toggle_button_clicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void pista5_toggle_button_clicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void pista3_toggle_button_clicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void pista6_toggle_button_clicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void calendar_on_action(ActionEvent event) {
+    }
     
-    
+
     
     
     
