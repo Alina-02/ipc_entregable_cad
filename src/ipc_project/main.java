994f7e55e0c2236b43432323cdeed32ae8699b60
dantr <dantr@10.236.11.170>
2023-05-22 19:46:20 +0200

    Se me olvido cambiar lo del main

--- src/ipc_project/main.java
+++ src/ipc_project/main.java
@@ -39,8 +39,7 @@ public class main extends Application{
        
         //======================================================================
         // 1- creación del grafo de escena a partir del fichero FXML
-        //FXMLLoader loader= new  FXMLLoader(getClass().getResource("/views/DisponibilidadDelDiaFXML.fxml"));
-        FXMLLoader loader= new  FXMLLoader(getClass().getResource("/views/registroFXML.fxml"));
+        FXMLLoader loader= new  FXMLLoader(getClass().getResource("/views/DisponibilidadDelDiaFXML.fxml"));
         Parent root = loader.load();
         //======================================================================
         // 2- creación de la escena con el nodo raiz del grafo de escena
